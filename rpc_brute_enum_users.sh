e852369cb66b9bda76ce931c52eccd93e479f637
Sh11td0wn <45502404+Sh11td0wn@users.noreply.github.com>
2019-08-13 18:12:20 -0300

    Update rpc_brute_enum_users.sh

--- rpc_brute_enum_users.sh
+++ rpc_brute_enum_users.sh
@@ -2,11 +2,14 @@
 #
 # Description
 #
-# This script connects to a given server via RPC, discovers it's SID and then enumerate the  machine / domain users via user SID bruteforce.
+# This script connects to a given server via RPC,
+# discovers it's SID and then enumerate the  machine / domain users via user SID bruteforce.
 #
-# This script relies on 'rpcclient' binary. Make sure to install 'smbclient' package on you Linux distro.
+# Obs. This script relies on 'rpcclient' binary. Make sure to install 'smbclient' package on you Linux distro.
 #
-# This piece of code is intended for educational purposes only. I'm not responsible for it's misuse.
+#                     *** Do NOT use this for illegal or malicious use ***                     
+#                By running this, YOU are using this program at YOUR OWN RISK.                 
+#            This software is provided "as is", WITHOUT ANY guarantees OR warranty.  
 #
 # Usage:
 # ./rpc_brute_enum_users.sh -s 192.168.0.15 -u user -p 'P@ssw0rd'
